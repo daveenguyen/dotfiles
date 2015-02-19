a87c0a1f38f9051d93e55640831f1fc988c470d2
Davee Nguyen <davee@daveenguyen.com>
2015-02-19 03:59:23 -0800

    Added more paths to fish paths and alias for MEMP stack.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,19 @@
-set fish_user_paths ~/.dotfiles/bin /usr/local/bin
+set fish_user_paths ~/.dotfiles/bin /usr/local/bin /usr/local/sbin /usr/local/opt/coreutils/libexec/gnubin (brew --prefix homebrew/php/php56)/sbin /Users/davee/Library/Android/sdk/tools /Users/davee/Library/Android/sdk/platform-tools
+
+set -x PHP_AUTOCONF (which autoconf)
+
+alias nginx.start='sudo launchctl load /Library/LaunchDaemons/homebrew.mxcl.nginx.plist'
+alias nginx.stop='sudo launchctl unload /Library/LaunchDaemons/homebrew.mxcl.nginx.plist'
+alias nginx.restart='nginx.stop & nginx.start'
+alias php-fpm.start="launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.php56.plist"
+alias php-fpm.stop="launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.php56.plist"
+alias php-fpm.restart='php-fpm.stop & php-fpm.start'
+alias mysql.start="launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist"
+alias mysql.stop="launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist"
+alias mysql.restart='mysql.stop & mysql.start'
+alias nginx.logs.error='tail -250f /usr/local/etc/nginx/logs/error.log'
+alias nginx.logs.access='tail -250f /usr/local/etc/nginx/logs/access.log'
+alias nginx.logs.default.access='tail -250f /usr/local/etc/nginx/logs/default.access.log'
+alias nginx.logs.default-ssl.access='tail -250f /usr/local/etc/nginx/logs/default-ssl.access.log'
+alias nginx.logs.phpmyadmin.error='tail -250f /usr/local/etc/nginx/logs/phpmyadmin.error.log'
+alias nginx.logs.phpmyadmin.access='tail -250f /usr/local/etc/nginx/logs/phpmyadmin.access.log'
